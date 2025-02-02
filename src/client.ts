aeed1f890b4ab7434ea033fab7c39d89c0588e26
Corncycle <caleb.mes@gmail.com>
2025-02-02 15:48:28 -0800

    scaffold resource loading

--- src/client.ts
+++ src/client.ts
@@ -2,10 +2,18 @@ import { io } from 'socket.io-client'
 import { connectChat } from './chat/chat'
 import { connectCanvas } from './canvas/canvas'
 import { initializeRapier } from './space/rapier.js'
+import { loadResources } from './loader.js'
 ;(async function initialize() {
   await initializeRapier()
 
+  await loadResources()
+
   const socket = io('http://localhost:9090')
+
+  socket.on('connect_error', (err) => {
+    console.log('failed to connect to socket server')
+    console.log(err)
+  })
 
   const space = connectCanvas(socket)
   connectChat(socket, space)
---
+++ src/loader.ts
@@ -0,0 +1,23 @@
+import * as THREE from 'three'
+
+const loader = document.querySelector('.loader-container')!
+const game = document.querySelector('.babl-container')!
+
+const textures = {
+  rockWall: {
+    diff: 'textures/rock-wall-diff-4k.jpg',
+    disp: 'textures/rock-wall-disp-4k.jpg',
+    nor: 'textures/rock-wall-nor-4k.jpg',
+  },
+}
+
+export const loadResources = async () => {
+  const textureLoader = new THREE.TextureLoader()
+
+  await new Promise((res) => {
+    setTimeout(res, 3000)
+  })
+
+  loader.classList.add('hidden')
+  game.classList.remove('hidden')
+}
